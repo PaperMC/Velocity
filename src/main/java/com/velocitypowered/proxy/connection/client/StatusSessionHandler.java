50c0426313a1d0d667108c9fb44ab5695155a646
MatrixTunnel <MatrixTunnel@users.noreply.github.com>
2018-07-31 06:13:31 -0700

    Also update version string

--- src/main/java/com/velocitypowered/proxy/connection/client/StatusSessionHandler.java
+++ src/main/java/com/velocitypowered/proxy/connection/client/StatusSessionHandler.java
@@ -36,7 +36,7 @@ public class StatusSessionHandler implements MinecraftSessionHandler {
 
         // Status request
         ServerPing ping = new ServerPing(
-                new ServerPing.Version(connection.getProtocolVersion(), "Velocity 1.10-1.12.2"),
+                new ServerPing.Version(connection.getProtocolVersion(), "Velocity 1.9-1.13"),
                 new ServerPing.Players(0, configuration.getShowMaxPlayers()),
                 configuration.getMotdComponent(),
                 null
