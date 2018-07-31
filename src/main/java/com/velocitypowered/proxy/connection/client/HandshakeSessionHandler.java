90f7a18e24b0a7ddfb632728f04d58e8253f2807
kashike <kashike@vq.lc>
2018-07-30 22:15:34 -0700

    Use a translation instead

--- src/main/java/com/velocitypowered/proxy/connection/client/HandshakeSessionHandler.java
+++ src/main/java/com/velocitypowered/proxy/connection/client/HandshakeSessionHandler.java
@@ -1,14 +1,14 @@
 package com.velocitypowered.proxy.connection.client;
 
 import com.google.common.base.Preconditions;
+import com.velocitypowered.proxy.connection.MinecraftConnection;
+import com.velocitypowered.proxy.connection.MinecraftSessionHandler;
 import com.velocitypowered.proxy.protocol.MinecraftPacket;
 import com.velocitypowered.proxy.protocol.ProtocolConstants;
 import com.velocitypowered.proxy.protocol.StateRegistry;
 import com.velocitypowered.proxy.protocol.packets.Disconnect;
 import com.velocitypowered.proxy.protocol.packets.Handshake;
-import com.velocitypowered.proxy.connection.MinecraftConnection;
-import com.velocitypowered.proxy.connection.MinecraftSessionHandler;
-import net.kyori.text.TextComponent;
+import net.kyori.text.TranslatableComponent;
 
 public class HandshakeSessionHandler implements MinecraftSessionHandler {
     private final MinecraftConnection connection;
@@ -35,7 +35,7 @@ public class HandshakeSessionHandler implements MinecraftSessionHandler {
                 connection.setState(StateRegistry.LOGIN);
                 connection.setProtocolVersion(handshake.getProtocolVersion());
                 if (!ProtocolConstants.isSupported(handshake.getProtocolVersion())) {
-                    connection.closeWith(Disconnect.create(TextComponent.of("Unsupported client")));
+                    connection.closeWith(Disconnect.create(TranslatableComponent.of("multiplayer.disconnect.outdated_client")));
                     return;
                 } else {
                     connection.setSessionHandler(new LoginSessionHandler(connection));
