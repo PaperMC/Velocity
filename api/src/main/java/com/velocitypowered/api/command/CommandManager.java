6e4b651c53ce81ccab32cf7ef8097d210e34b39c
kashike <kashike@vq.lc>
2018-08-14 03:55:12 -0700

    allow vararg aliases

--- api/src/main/java/com/velocitypowered/api/command/CommandManager.java
+++ api/src/main/java/com/velocitypowered/api/command/CommandManager.java
@@ -4,9 +4,9 @@ package com.velocitypowered.api.command;
  * Represents an interface to register a command executor with the proxy.
  */
 public interface CommandManager {
-    void registerCommand(String name, Command command);
+    void register(Command command, String... aliases);
 
-    void unregisterCommand(String name);
+    void unregister(String alias);
 
     boolean execute(CommandSource source, String cmdLine);
 }
