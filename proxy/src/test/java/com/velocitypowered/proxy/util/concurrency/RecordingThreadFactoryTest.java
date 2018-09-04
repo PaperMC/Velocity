c30cc664613e216f2f71f2d3fe5c67f5b03aa769
Andrew Steinborn <git@steinborn.me>
2018-09-03 23:30:18 -0400

    Test logic when exception is thrown in thread

--- proxy/src/test/java/com/velocitypowered/proxy/util/concurrency/RecordingThreadFactoryTest.java
+++ proxy/src/test/java/com/velocitypowered/proxy/util/concurrency/RecordingThreadFactoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,6 +29,38 @@ class RecordingThreadFactoryTest {
         assertFalse(factory.currentlyInFactory());
         assertEquals(1, factory.size());
         endThread.countDown();
+
+        // Wait a little bit to ensure the thread got shut down
+        Thread.sleep(10);
+        assertEquals(0, factory.size());
+    }
+
+    @Test
+    void cleanUpAfterExceptionThrown() throws Exception {
+        CountDownLatch started = new CountDownLatch(1);
+        CountDownLatch endThread = new CountDownLatch(1);
+        CountDownLatch hasEnded = new CountDownLatch(1);
+        RecordingThreadFactory factory = new RecordingThreadFactory((ThreadFactory) r -> {
+            Thread t = new Thread(r);
+            t.setUncaughtExceptionHandler((t1, e) -> hasEnded.countDown());
+            return t;
+        });
+        factory.newThread(() -> {
+            started.countDown();
+            assertTrue(factory.currentlyInFactory());
+            assertEquals(1, factory.size());
+            try {
+                endThread.await();
+            } catch (InterruptedException e) {
+                fail(e);
+            }
+            throw new RuntimeException("");
+        }).start();
+        started.await();
+        assertFalse(factory.currentlyInFactory());
+        assertEquals(1, factory.size());
+        endThread.countDown();
+        hasEnded.await();
 
         // Wait a little bit to ensure the thread got shut down
         Thread.sleep(10);
