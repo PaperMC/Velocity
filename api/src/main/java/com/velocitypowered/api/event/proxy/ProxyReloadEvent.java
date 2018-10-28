70ebb42afcc7de0636042f3a736291268e7b417f
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-10-28 18:33:07 +0200

    Add ProxyReloadEvent

---
+++ api/src/main/java/com/velocitypowered/api/event/proxy/ProxyReloadEvent.java
@@ -0,0 +1,12 @@
+package com.velocitypowered.api.event.proxy;
+
+/**
+ * This event is fired by the proxy after its configuration is reloaded.
+ */
+public final class ProxyReloadEvent {
+
+  @Override
+  public String toString() {
+    return "ProxyReloadEvent";
+  }
+}
