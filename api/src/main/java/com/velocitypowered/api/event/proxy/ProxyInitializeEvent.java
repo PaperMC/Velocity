dcb986bebdc030333b4afe5f89d987e5ca709487
Lars Artmann <LartyHD@gmx.de>
2018-11-22 05:04:56 +0100

    Added ProxyInitializeEvent to NamedEvent

--- api/src/main/java/com/velocitypowered/api/event/proxy/ProxyInitializeEvent.java
+++ api/src/main/java/com/velocitypowered/api/event/proxy/ProxyInitializeEvent.java
@@ -1,13 +1,9 @@
 package com.velocitypowered.api.event.proxy;
 
+import com.velocitypowered.api.event.NamedEvent;
+
 /**
  * This event is fired by the proxy after plugins have been loaded but before the proxy starts
  * accepting connections.
  */
-public final class ProxyInitializeEvent {
-
-  @Override
-  public String toString() {
-    return "ProxyInitializeEvent";
-  }
-}
+public final class ProxyInitializeEvent extends NamedEvent {}
