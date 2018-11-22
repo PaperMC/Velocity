0b346cf1a773ff38cddc5a492ef7a2af50c2c87b
Lars Artmann <LartyHD@gmx.de>
2018-11-22 05:14:17 +0100

    Added a public constructor to ProxyInitializeEvent

--- api/src/main/java/com/velocitypowered/api/event/proxy/ProxyInitializeEvent.java
+++ api/src/main/java/com/velocitypowered/api/event/proxy/ProxyInitializeEvent.java
@@ -6,4 +6,8 @@ import com.velocitypowered.api.event.NamedEvent;
  * This event is fired by the proxy after plugins have been loaded but before the proxy starts
  * accepting connections.
  */
-public final class ProxyInitializeEvent extends NamedEvent {}
+public final class ProxyInitializeEvent extends NamedEvent {
+
+  public ProxyShutdownEvent {}
+
+}
