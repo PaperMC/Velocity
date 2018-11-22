12bf8ec5c4052165715c7b072615ee96140738f0
Lars Artmann <LartyHD@gmx.de>
2018-11-22 05:01:28 +0100

    Added NamedEvent

--- api/src/main/java/com/velocitypowered/api/event/proxy/ProxyShutdownEvent.java
+++ api/src/main/java/com/velocitypowered/api/event/proxy/ProxyShutdownEvent.java
@@ -1,13 +1,9 @@
 package com.velocitypowered.api.event.proxy;
 
+import com.velocitypowered.api.event.NamedEvent;
+
 /**
  * This event is fired by the proxy after the proxy has stopped accepting connections but before the
  * proxy process exits.
  */
-public final class ProxyShutdownEvent {
-
-  @Override
-  public String toString() {
-    return "ProxyShutdownEvent";
-  }
-}
+public final class ProxyShutdownEvent extends NamedEvent {}
