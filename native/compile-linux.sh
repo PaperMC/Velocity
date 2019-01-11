d8e7f875068e2305dec1d0ab4de43dd69803c8ba
Andrew Steinborn <git@steinborn.me>
2019-01-11 14:15:07 -0500

    Forgot to update the JNI compile script for Linux.

--- native/compile-linux.sh
+++ native/compile-linux.sh
@@ -4,7 +4,7 @@
 MBEDTLS_ROOT=mbedtls
 CFLAGS="-O3 -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ -fPIC -shared"
 gcc $CFLAGS -lz src/main/c/jni_util.c src/main/c/jni_zlib_deflate.c src/main/c/jni_zlib_inflate.c \
-    -o src/main/resources/linux_x64/velocity-compress.so
+    src/main/c/jni_zlib_common.c -o src/main/resources/linux_x64/velocity-compress.so
 gcc $CFLAGS  -I $MBEDTLS_ROOT/include -shared $MBEDTLS_ROOT/library/aes.c $MBEDTLS_ROOT/library/aesni.c \
     $MBEDTLS_ROOT/library/platform.c $MBEDTLS_ROOT/library/platform_util.c src/main/c/jni_util.c src/main/c/jni_cipher.c \
     -o src/main/resources/linux_x64/velocity-cipher.so
