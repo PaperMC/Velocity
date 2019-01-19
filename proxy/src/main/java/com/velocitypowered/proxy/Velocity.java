865f9e5ef9cd133c309bafd2ee628a44a274e471
Andrew Steinborn <git@steinborn.me>
2019-01-19 03:02:50 -0500

    Bump the pool buffer size to 4MiB temporarily

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -14,14 +14,14 @@ public class Velocity {
     System.setProperty("java.awt.headless", "true");
 
     // By default, Netty allocates 16MiB arenas for the PooledByteBufAllocator. This is too much
-    // memory for Minecraft, which imposes a maximum packet size of 2MiB! We'll use 2MiB as a more
+    // memory for Minecraft, which imposes a maximum packet size of 2MiB! We'll use 4MiB as a more
     // sane default.
     //
     // Note: io.netty.allocator.pageSize << io.netty.allocator.maxOrder is the formula used to
     // compute the chunk size. We lower maxOrder from its default of 11 to 8. (We also use a null
     // check, so that the user is free to choose another setting if need be.)
     if (System.getProperty("io.netty.allocator.maxOrder") == null) {
-      System.setProperty("io.netty.allocator.maxOrder", "8");
+      System.setProperty("io.netty.allocator.maxOrder", "9");
     }
   }
 
