226074a0df40d8989278cdd4fbff6439b20f6236
Andrew Steinborn <git@steinborn.me>
2019-06-29 14:35:28 -0400

    Add suggestion for reconnecting for "not online-mode" message

--- proxy/src/main/java/com/velocitypowered/proxy/util/VelocityMessages.java
+++ proxy/src/main/java/com/velocitypowered/proxy/util/VelocityMessages.java
@@ -7,7 +7,13 @@ import net.kyori.text.format.TextColor;
 public class VelocityMessages {
 
   public static final Component ONLINE_MODE_ONLY = TextComponent
-      .of("This server only accepts connections from online-mode clients.", TextColor.RED);
+      .builder("This server only accepts connections from online-mode clients.")
+      .color(TextColor.RED)
+      .append(
+          TextComponent.of("\n\nDid you change your username? Sign out of Minecraft, sign back in, "
+              + "and try again.", TextColor.GRAY)
+      )
+      .build();
   public static final Component NO_PROXY_BEHIND_PROXY = TextComponent
       .of("Running Velocity behind Velocity isn't supported.", TextColor.RED);
   public static final Component NO_AVAILABLE_SERVERS = TextComponent
