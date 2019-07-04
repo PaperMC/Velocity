2b5538585cdc75d8dc7b7401c26a50e361e060ea
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2019-07-04 14:02:08 +0200

    Cache components of translations with no arguments.

--- proxy/src/main/java/com/velocitypowered/proxy/translation/TranslationManager.java
+++ proxy/src/main/java/com/velocitypowered/proxy/translation/TranslationManager.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.IllegalFormatException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -33,10 +34,26 @@ public class TranslationManager {
    */
   class Translation {
 
-    private final String plain;
+    final String plain;
 
     Translation(String plain) {
       this.plain = plain;
+    }
+  }
+
+  /**
+   * Represents a fixed translation.
+   */
+  class FixedTranslation extends Translation {
+
+    final Component component;
+    final boolean plainComponent;
+
+    FixedTranslation(String plain, Component component) {
+      super(plain);
+      this.component = component;
+      this.plainComponent = component instanceof TextComponent
+          && component.children().isEmpty() && !component.hasStyling();
     }
   }
 
@@ -47,9 +64,8 @@ public class TranslationManager {
 
     final String json;
 
-    ComponentTranslation(String json) {
-      super(PlainComponentSerializer.INSTANCE
-          .serialize(GsonComponentSerializer.INSTANCE.deserialize(json)));
+    ComponentTranslation(String json, String plain) {
+      super(plain);
       this.json = json;
     }
   }
@@ -67,7 +83,14 @@ public class TranslationManager {
             .computeIfAbsent(locale, locale1 -> new HashMap<>());
     for (String key : resourceBundle.keySet()) {
       String value = resourceBundle.getString(key);
-      translations.put(key, new Translation(value));
+
+      Translation translation;
+      if (!hasArguments(value)) {
+        translation = new FixedTranslation(value, TextComponent.of(value));
+      } else {
+        translation = new Translation(value);
+      }
+      translations.put(key, translation);
     }
     if (fallback == null) {
       fallback = translations;
@@ -91,12 +114,38 @@ public class TranslationManager {
       if (json.isJsonNull()) {
         continue;
       }
-      Translation translation = json.isJsonPrimitive() ? new Translation(json.getAsString()) :
-          new ComponentTranslation(GSON.toJson(json));
+      Translation translation;
+      String value = json.isJsonPrimitive() ? json.getAsString() : GSON.toJson(json);
+      if (!hasArguments(value)) {
+        if (json.isJsonPrimitive()) {
+          translation = new FixedTranslation(value, TextComponent.of(value));
+        } else {
+          translation = new FixedTranslation(value, GsonComponentSerializer.INSTANCE
+              .deserialize(value));
+        }
+      } else {
+        if (json.isJsonPrimitive()) {
+          translation = new Translation(value);
+        } else {
+          String plain = PlainComponentSerializer.INSTANCE
+              .serialize(GsonComponentSerializer.INSTANCE.deserialize(value));
+          translation = new ComponentTranslation(value, plain);
+        }
+      }
       translations.put(key, translation);
     }
     if (fallback == null) {
       fallback = translations;
+    }
+  }
+
+  private boolean hasArguments(String format) {
+    try {
+      //noinspection RedundantStringFormatCall,ResultOfMethodCallIgnored
+      String.format(format); // Check if any arguments are required for the translation
+      return false;
+    } catch (IllegalFormatException ex) {
+      return true;
     }
   }
 
@@ -123,7 +172,7 @@ public class TranslationManager {
    */
   private @Nullable String translateIfFound(Locale locale, String key, Object... arguments) {
     Translation translation = getTranslation(locale, key);
-    return translation == null ? null : String.format(translation.plain, arguments);
+    return translation == null ? null : String.format(locale, translation.plain, arguments);
   }
 
   private @Nullable Translation getTranslation(Locale locale, String key) {
@@ -170,13 +219,24 @@ public class TranslationManager {
           arguments[i] = PlainComponentSerializer.INSTANCE.serialize(translated);
         }
         TextComponent.Builder builder;
-        if (translation instanceof ComponentTranslation) {
+        if (translation instanceof FixedTranslation) {
+          FixedTranslation fixedTranslation = (FixedTranslation) translation;
+          Component fixed = fixedTranslation.component;
+          if (children.isEmpty() && !component.hasStyling()) {
+            return fixed;
+          }
+          if (fixedTranslation.plainComponent) {
+            builder = TextComponent.builder(((TextComponent) fixed).content());
+          } else {
+            builder = TextComponent.builder().append(fixed);
+          }
+        } else if (translation instanceof ComponentTranslation) {
           ComponentTranslation componentTranslation = (ComponentTranslation) translation;
           Component formatted = GsonComponentSerializer.INSTANCE
-              .deserialize(String.format(componentTranslation.json, arguments));
+              .deserialize(String.format(locale, componentTranslation.json, arguments));
           builder = TextComponent.builder().append(formatted);
         } else {
-          builder = TextComponent.builder(String.format(translation.plain, arguments));
+          builder = TextComponent.builder(String.format(locale, translation.plain, arguments));
         }
         return builder
                 .style(component.style())
