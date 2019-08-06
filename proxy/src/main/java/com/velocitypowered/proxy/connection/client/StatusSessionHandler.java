34a72be40a855f05c9492fe0c3b70380a3dfc504
Alexander Staeding <alexanderstaeding@gmail.com>
2019-08-05 22:27:53 -0400

    Fixed indentation

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/StatusSessionHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/StatusSessionHandler.java
@@ -23,84 +23,84 @@ import java.util.concurrent.ExecutionException;
 
 public class StatusSessionHandler implements MinecraftSessionHandler {
 
-    private final VelocityServer server;
-    private final MinecraftConnection connection;
-    private final InboundConnection inboundWrapper;
+  private final VelocityServer server;
+  private final MinecraftConnection connection;
+  private final InboundConnection inboundWrapper;
 
-    StatusSessionHandler(VelocityServer server, MinecraftConnection connection,
-                         InboundConnection inboundWrapper) {
-        this.server = server;
-        this.connection = connection;
-        this.inboundWrapper = inboundWrapper;
-    }
+  StatusSessionHandler(VelocityServer server, MinecraftConnection connection,
+                       InboundConnection inboundWrapper) {
+    this.server = server;
+    this.connection = connection;
+    this.inboundWrapper = inboundWrapper;
+  }
 
-    private ServerPing createInitialPing() {
-        VelocityConfiguration configuration = server.getConfiguration();
-        ProtocolVersion shownVersion = ProtocolVersion.isSupported(connection.getProtocolVersion())
-                ? connection.getProtocolVersion() : ProtocolVersion.MAXIMUM_VERSION;
-        return new ServerPing(
-                new ServerPing.Version(shownVersion.getProtocol(),
-                        "Velocity " + ProtocolVersion.SUPPORTED_VERSION_STRING),
-                new ServerPing.Players(server.getPlayerCount(), configuration.getShowMaxPlayers(),
-                        ImmutableList.of()),
-                configuration.getMotdComponent(),
-                configuration.getFavicon().orElse(null),
-                getModInfo(configuration.isAnnounceForge())
-        );
-    }
+  private ServerPing createInitialPing() {
+    VelocityConfiguration configuration = server.getConfiguration();
+    ProtocolVersion shownVersion = ProtocolVersion.isSupported(connection.getProtocolVersion())
+        ? connection.getProtocolVersion() : ProtocolVersion.MAXIMUM_VERSION;
+    return new ServerPing(
+        new ServerPing.Version(shownVersion.getProtocol(),
+            "Velocity " + ProtocolVersion.SUPPORTED_VERSION_STRING),
+        new ServerPing.Players(server.getPlayerCount(), configuration.getShowMaxPlayers(),
+            ImmutableList.of()),
+        configuration.getMotdComponent(),
+        configuration.getFavicon().orElse(null),
+        getModInfo(configuration.isAnnounceForge())
+    );
+  }
 
-    private ModInfo getModInfo(boolean isAnnounceForge) {
-        for (String serverName : server.getConfiguration().getAttemptModInfoOrder()) {
-            Optional<RegisteredServer> registeredServer = server.getServer(serverName);
-            if (!registeredServer.isPresent()) continue;
-            try {
-                ServerPing serverPing = registeredServer.get().ping().get();
-                if (serverPing == null) continue;
-                Optional<ModInfo> modInfo = serverPing.getModinfo();
-                if (!modInfo.isPresent()) continue;
-                return modInfo.get();
-            } catch (InterruptedException | ExecutionException ignored) {
-            }
-        }
-        return isAnnounceForge ? ModInfo.DEFAULT : null;
+  private ModInfo getModInfo(boolean isAnnounceForge) {
+    for (String serverName : server.getConfiguration().getAttemptModInfoOrder()) {
+      Optional<RegisteredServer> registeredServer = server.getServer(serverName);
+      if (!registeredServer.isPresent()) continue;
+      try {
+        ServerPing serverPing = registeredServer.get().ping().get();
+        if (serverPing == null) continue;
+        Optional<ModInfo> modInfo = serverPing.getModinfo();
+        if (!modInfo.isPresent()) continue;
+        return modInfo.get();
+      } catch (InterruptedException | ExecutionException ignored) {
+      }
     }
+    return isAnnounceForge ? ModInfo.DEFAULT : null;
+  }
 
-    @Override
-    public boolean handle(LegacyPing packet) {
-        ServerPing initialPing = createInitialPing();
-        ProxyPingEvent event = new ProxyPingEvent(inboundWrapper, initialPing);
-        server.getEventManager().fire(event)
-                .thenRunAsync(() -> {
-                    connection.closeWith(LegacyDisconnect.fromServerPing(event.getPing(),
-                            packet.getVersion()));
-                }, connection.eventLoop());
-        return true;
-    }
+  @Override
+  public boolean handle(LegacyPing packet) {
+    ServerPing initialPing = createInitialPing();
+    ProxyPingEvent event = new ProxyPingEvent(inboundWrapper, initialPing);
+    server.getEventManager().fire(event)
+        .thenRunAsync(() -> {
+          connection.closeWith(LegacyDisconnect.fromServerPing(event.getPing(),
+              packet.getVersion()));
+        }, connection.eventLoop());
+    return true;
+  }
 
-    @Override
-    public boolean handle(StatusPing packet) {
-        connection.closeWith(packet);
-        return true;
-    }
+  @Override
+  public boolean handle(StatusPing packet) {
+    connection.closeWith(packet);
+    return true;
+  }
 
-    @Override
-    public boolean handle(StatusRequest packet) {
-        ServerPing initialPing = createInitialPing();
-        ProxyPingEvent event = new ProxyPingEvent(inboundWrapper, initialPing);
-        server.getEventManager().fire(event)
-                .thenRunAsync(
-                        () -> {
-                            StringBuilder json = new StringBuilder();
-                            VelocityServer.GSON.toJson(event.getPing(), json);
-                            connection.write(new StatusResponse(json));
-                        },
-                        connection.eventLoop());
-        return true;
-    }
+  @Override
+  public boolean handle(StatusRequest packet) {
+    ServerPing initialPing = createInitialPing();
+    ProxyPingEvent event = new ProxyPingEvent(inboundWrapper, initialPing);
+    server.getEventManager().fire(event)
+        .thenRunAsync(
+            () -> {
+              StringBuilder json = new StringBuilder();
+              VelocityServer.GSON.toJson(event.getPing(), json);
+              connection.write(new StatusResponse(json));
+            },
+            connection.eventLoop());
+    return true;
+  }
 
-    @Override
-    public void handleUnknown(ByteBuf buf) {
-        // what even is going on?
-        connection.close();
-    }
+  @Override
+  public void handleUnknown(ByteBuf buf) {
+    // what even is going on?
+    connection.close();
+  }
 }
