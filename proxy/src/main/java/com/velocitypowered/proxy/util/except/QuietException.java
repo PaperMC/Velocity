68e5372b2e682d19068a4a221ce427847714d657
Andrew Steinborn <git@steinborn.me>
2019-11-27 01:04:48 -0500

    QuietException should be a RuntimeException

--- proxy/src/main/java/com/velocitypowered/proxy/util/except/QuietException.java
+++ proxy/src/main/java/com/velocitypowered/proxy/util/except/QuietException.java
@@ -4,7 +4,7 @@ package com.velocitypowered.proxy.util.except;
  * A special-purpose exception thrown when we want to indicate an error condition but do not want
  * to see a large stack trace in logs.
  */
-public class QuietException extends Exception {
+public class QuietException extends RuntimeException {
 
   public QuietException(String message) {
     super(message);
