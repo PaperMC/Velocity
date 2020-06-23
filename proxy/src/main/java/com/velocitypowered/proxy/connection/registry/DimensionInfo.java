31a7e22719ea307ba6a45e3bd490cbb42347ffc1
Andrew Steinborn <git@steinborn.me>
2020-06-23 07:59:05 -0400

    Fix compile error with JDK 8

--- proxy/src/main/java/com/velocitypowered/proxy/connection/registry/DimensionInfo.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/registry/DimensionInfo.java
@@ -21,13 +21,13 @@ public final class DimensionInfo {
     this.registryIdentifier = Preconditions.checkNotNull(
             registryIdentifier, "registryIdentifier cannot be null");
     Preconditions.checkArgument(
-            registryIdentifier.length() > 0 && !registryIdentifier.isBlank(),
-            "registryIdentifier cannot be empty");
+        registryIdentifier.length() > 0,
+        "registryIdentifier cannot be empty");
     this.levelName = Preconditions.checkNotNull(
             levelName, "levelName cannot be null");
     Preconditions.checkArgument(
-            levelName.length() > 0 && !levelName.isBlank(),
-            "registryIdentifier cannot be empty");
+        levelName.length() > 0,
+        "registryIdentifier cannot be empty");
     this.isFlat = isFlat;
     this.isDebugType = isDebugType;
   }
