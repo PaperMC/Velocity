b2d9e11217f2440808e8e6baddafa0595e47365d
Andrew Steinborn <git@steinborn.me>
2020-08-06 07:42:42 -0400

    Fix packet decode logging not giving useful errors
    See #349 for context

--- proxy/src/main/java/com/velocitypowered/proxy/connection/MinecraftConnection.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/MinecraftConnection.java
@@ -157,9 +157,9 @@ public class MinecraftConnection extends ChannelInboundHandlerAdapter {
           logger.error("{}: read timed out", association);
         } else {
           boolean isQuietDecoderException = cause instanceof QuietDecoderException;
-          boolean willLogQuietDecoderException = isQuietDecoderException
+          boolean willLogQuietDecoderException = MinecraftDecoder.DEBUG || (isQuietDecoderException
               && !(sessionHandler instanceof LoginSessionHandler)
-              && !(sessionHandler instanceof HandshakeSessionHandler);
+              && !(sessionHandler instanceof HandshakeSessionHandler));
           if (willLogQuietDecoderException) {
             logger.error("{}: exception encountered in {}", association, sessionHandler, cause);
           } else if (isQuietDecoderException) {
