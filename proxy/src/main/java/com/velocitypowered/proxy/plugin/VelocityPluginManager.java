59d8bd4c78a2a6baaeaecb690d2d22dce41e49d4
Andrew Steinborn <git@steinborn.me>
2020-10-11 14:36:56 -0400

    Plugins need to be stored in a LinkedHashMap too.

--- proxy/src/main/java/com/velocitypowered/proxy/plugin/VelocityPluginManager.java
+++ proxy/src/main/java/com/velocitypowered/proxy/plugin/VelocityPluginManager.java
@@ -41,7 +41,7 @@ public class VelocityPluginManager implements PluginManager {
 
   private static final Logger logger = LogManager.getLogger(VelocityPluginManager.class);
 
-  private final Map<String, PluginContainer> plugins = new HashMap<>();
+  private final Map<String, PluginContainer> plugins = new LinkedHashMap<>();
   private final Map<Object, PluginContainer> pluginInstances = new IdentityHashMap<>();
   private final VelocityServer server;
 
