b19cf16345c30d03aeed4f96fe3cd249566486e3
Five (Xer) <admin@fivepb.me>
2021-02-18 22:02:26 +0100

    Snapshot 21w07a

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/ClientSettingsWrapper.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/ClientSettingsWrapper.java
@@ -26,7 +26,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 public class ClientSettingsWrapper implements PlayerSettings {
 
   static final PlayerSettings DEFAULT = new ClientSettingsWrapper(
-      new ClientSettings("en_US", (byte) 10, 0, true, (short) 127, 1));
+      new ClientSettings("en_US", (byte) 10, 0, true, (short) 127, 1, true));
 
   private final ClientSettings settings;
   private final SkinParts parts;
