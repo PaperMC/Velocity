737a4b4ef6cbfbe6ccce5591a7a9e3ea09399c95
Hugo Manrique <contact@hugmanrique.me>
2021-02-24 22:24:59 +0100

    Test SimpleCommand suggestions

--- proxy/src/test/java/com/velocitypowered/proxy/command/SimpleCommandTests.java
+++ proxy/src/test/java/com/velocitypowered/proxy/command/SimpleCommandTests.java
@@ -1,10 +1,12 @@
 package com.velocitypowered.proxy.command;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.command.SimpleCommand;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.Test;
@@ -97,5 +99,149 @@ public class SimpleCommandTests extends CommandTestSuite {
     assertExecuted("hello this  is   a sentence");
 
     assertEquals(5, execCount.get());
+  }
+
+  @Test
+  void testAliasSuggestions() {
+    final CommandMeta meta = manager.metaBuilder("hello").build();
+    manager.register(meta, new SimpleCommand() {
+      @Override
+      public void execute(final Invocation invocation) {
+        fail();
+      }
+
+      @Override
+      public List<String> suggest(final Invocation invocation) {
+        fail("Alias suggestion must not call command for suggestions");
+        return null;
+      }
+    });
+
+    // TODO Isn't this testing CommandManager instead of SimpleCommand?
+    assertSuggestions("", "hello");
+    assertSuggestions("hel", "hello");
+    assertSuggestions("HE", "hello");
+
+    assertSuggestions("hello"); // once complete, no suggestions
+    assertSuggestions("a");
+  }
+
+  final class WorldSuggestionCommand implements SimpleCommand {
+
+    final AtomicInteger callCount = new AtomicInteger();
+    final AtomicReference<String[]> expectedArgs = new AtomicReference<>();
+
+    @Override
+    public void execute(final Invocation invocation) {
+      fail();
+    }
+
+    @Override
+    public List<String> suggest(final Invocation invocation) {
+      assertEquals(dummySource, invocation.source());
+      assertEquals("hello", invocation.alias());
+      assertArrayEquals(expectedArgs.get(), invocation.arguments());
+      return Collections.singletonList("world");
+    }
+  }
+
+  @Test
+  void testArgumentSuggestionsAfterAlias() {
+    final CommandMeta meta = manager.metaBuilder("hello").build();
+    final WorldSuggestionCommand command = new WorldSuggestionCommand();
+    manager.register(meta, command);
+
+    command.expectedArgs.set(new String[0]);
+    assertSuggestions("hello ", "world");
+    assertSuggestions("Hello ", "world");
+
+    command.expectedArgs.set(new String[] { "cyber" });
+    assertSuggestions("hello cyber", "world");
+
+    command.expectedArgs.set(new String[] { "world" });
+    assertSuggestions("hello world"); // exact match, no suggestion
+
+    command.expectedArgs.set(new String[] { "World" });
+    assertSuggestions("hello World", "world"); // case sensitive
+
+    assertEquals(5, command.callCount.get());
+  }
+
+  @Test
+  void testArgumentSuggestionsWithinArguments() {
+    final CommandMeta meta = manager.metaBuilder("hello").build();
+    final WorldSuggestionCommand command = new WorldSuggestionCommand();
+    manager.register(meta, command);
+
+    // Unlike execute, the suggest method receives the non-trimmed command line
+    command.expectedArgs.set(new String[]{ "wonderful", "" });
+    assertSuggestions("hello wonderful ", "world");
+
+    command.expectedArgs.set(new String[]{ "dear", "", "" });
+    assertSuggestions("hello dear  ", "world");
+
+    command.expectedArgs.set(new String[]{ "dear", "world" }); // Only single-arg can match exactly
+    assertSuggestions("hello dear world", "world");
+
+    command.expectedArgs.set(new String[]{ "Dear", "wor" });
+    assertSuggestions("hello Dear wor", "world");
+
+    command.expectedArgs.set(new String[]{ "I'm", "", "", "in", "the", "", "", "" });
+    assertSuggestions("hello I'm   in the   ", "world");
+  }
+
+  @Test
+  void testStringArgumentSuggestion() {
+    final AtomicReference<String[]> expectedArgs = new AtomicReference<>();
+
+    final CommandMeta meta = manager.metaBuilder("This").build();
+    manager.register(meta, new SimpleCommand() {
+      @Override
+      public void execute(final Invocation invocation) {
+        fail();
+      }
+
+      @Override
+      public List<String> suggest(final Invocation invocation) {
+        assertEquals("this", invocation.alias());
+        assertArrayEquals(expectedArgs.get(), invocation.arguments());
+        return Arrays.asList("is a sentence", "is another sentence");
+      }
+    });
+
+    expectedArgs.set(new String[0]);
+    assertSuggestions("This ", "is a sentence", "is another sentence");
+
+    expectedArgs.set(new String[]{ "is", "" });
+    assertSuggestions("This is ", "is a sentence", "is another sentence");
+
+    expectedArgs.set(new String[] { "is", "a" });
+    assertSuggestions("This is a", "is a sentence", "is another sentence");
+
+    // Exact match
+    expectedArgs.set(new String[]{ "is", "a", "sentence" });
+    assertSuggestions("This is a sentence", "is another sentence");
+
+    // Other exact match
+    expectedArgs.set(new String[]{ "is", "another", "sentence" });
+    assertSuggestions("This is another sentence", "is a sentence");
+  }
+
+  @Test
+  void testArgumentSuggestionsAreSortedAlphabetically() {
+    final CommandMeta meta = manager.metaBuilder("hello").build();
+    manager.register(meta, new SimpleCommand() {
+      @Override
+      public void execute(final Invocation invocation) {
+        fail();
+      }
+
+      @Override
+      public List<String> suggest(final Invocation invocation) {
+        return Arrays.asList("world", "people", "World"); // unordered intentionally
+      }
+    });
+
+    assertSuggestions("hello ", "people", "World", "world");
   }
 }
