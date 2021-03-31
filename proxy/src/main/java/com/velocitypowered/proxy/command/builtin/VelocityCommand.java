c747120b20be1853ee02e7b2b67871249a88aa6d
Andrew Steinborn <git@steinborn.me>
2021-03-31 16:04:06 -0400

    Relicense the Velocity proxy under the terms of GPLv3 (#448)

--- proxy/src/main/java/com/velocitypowered/proxy/command/builtin/VelocityCommand.java
+++ proxy/src/main/java/com/velocitypowered/proxy/command/builtin/VelocityCommand.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 Velocity Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.velocitypowered.proxy.command.builtin;
 
 import com.google.common.base.Joiner;
@@ -211,8 +228,8 @@ public class VelocityCommand implements SimpleCommand {
           .append(Component.text(version.getVersion()).decoration(TextDecoration.BOLD, false))
           .build();
       TextComponent copyright = Component
-          .text("Copyright 2018-2021 " + version.getVendor() + ". " + version.getName()
-              + " is freely licensed under the terms of the MIT License.");
+          .text("Copyright 2018-2020 " + version.getVendor() + ". " + version.getName()
+              + " is licensed under the terms of the GNU General Public License v3.");
       source.sendMessage(Identity.nil(), velocity);
       source.sendMessage(Identity.nil(), copyright);
 
