01e69557b854bcf7a0c4b627b465753cde3095e8
Hugo Manrique <contact@hugmanrique.me>
2021-04-01 18:38:46 +0200

    Add BrigadierCommand suggestion tests

--- proxy/src/test/java/com/velocitypowered/proxy/command/BrigadierCommandTests.java
+++ proxy/src/test/java/com/velocitypowered/proxy/command/BrigadierCommandTests.java
@@ -1,11 +1,12 @@
 package com.velocitypowered.proxy.command;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.StringRange;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
@@ -13,6 +14,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Test;
 
 public class BrigadierCommandTests extends CommandTestSuite {
+
+  // Execution
 
   @Test
   void testAliasLevelNodeExecutionDoesNotForward() {
@@ -112,5 +115,70 @@ public class BrigadierCommandTests extends CommandTestSuite {
     manager.register(new BrigadierCommand(node));
 
     assertNotForwarded("sell 456");
+  }
+
+  // Suggestions
+
+  @Test
+  void testArgumentSuggestions() {
+    final LiteralCommandNode<CommandSource> node = LiteralArgumentBuilder
+            .<CommandSource>literal("hello")
+            .then(RequiredArgumentBuilder
+                .<CommandSource, String>argument("argument", StringArgumentType.word())
+                    .suggests((context, builder) ->
+                        builder
+                          .suggest("foo")
+                          .suggest("bar")
+                          .suggest("baz")
+                          .buildFuture()
+                    )
+            )
+            .build();
+    manager.register(new BrigadierCommand(node));
+
+    assertSuggestions("hello ", "bar", "baz", "foo");
+    assertSuggestions("hello ba", "bar", "baz", "foo");
+    assertSuggestions("hello bar", "baz", "foo");
+  }
+
+  @Test
+  void testNoSuggestionIfImpermissible() {
+    final LiteralCommandNode<CommandSource> node = LiteralArgumentBuilder
+            .<CommandSource>literal("parent")
+            .then(LiteralArgumentBuilder
+                    .<CommandSource>literal("child")
+                    .requiresWithContext((context, reader) -> {
+                      assertEquals(StringRange.between(0, 7), context.getRange());
+                      assertEquals(2, context.getNodes().size());
+                      assertEquals(7, reader.getCursor());
+                      return false;
+                    })
+            )
+            .build();
+    manager.register(new BrigadierCommand(node));
+
+    assertSuggestions("parent ");
+    assertSuggestions("parent chi");
+  }
+
+  @Test
+  void testNoSuggestionIfImpermissible_parse() {
+    final LiteralCommandNode<CommandSource> node = LiteralArgumentBuilder
+            .<CommandSource>literal("parent")
+            .then(LiteralArgumentBuilder
+                    .<CommandSource>literal("child")
+                    .requiresWithContext((context, reader) -> {
+                      // Called twice with different ranges
+                      // The first checks the child node can be considered during parsing.
+                      // The second checks if we can provide suggestions to the source when
+                      // the parent node of the suggestion context is the "parent" literal.
+                      assertEquals(2, context.getNodes().size());
+                      return false;
+                    })
+            )
+            .build();
+    manager.register(new BrigadierCommand(node));
+
+    assertSuggestions("parent child");
   }
 }
