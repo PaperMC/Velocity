6134e7cac89079d99946ba7dfe48d6cf713abc84
Andrew Steinborn <git@steinborn.me>
2021-07-05 13:24:39 -0400

    Revert maxOrder tuning
    This is no longer applicable with 1.17.1-rc2 (and is a sign 1.18 will send very large amounts of chunk data over the wire).

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -35,17 +35,6 @@ public class Velocity {
     // How inconvenient. Force AWT to work with its head chopped off.
     System.setProperty("java.awt.headless", "true");
 
-    // By default, Netty allocates 16MiB arenas for the PooledByteBufAllocator. This is too much
-    // memory for Minecraft, which imposes a maximum packet size of 2MiB! We'll use 4MiB as a more
-    // sane default.
-    //
-    // Note: io.netty.allocator.pageSize << io.netty.allocator.maxOrder is the formula used to
-    // compute the chunk size. We lower maxOrder from its default of 11 to 9. (We also use a null
-    // check, so that the user is free to choose another setting if need be.)
-    if (System.getProperty("io.netty.allocator.maxOrder") == null) {
-      System.setProperty("io.netty.allocator.maxOrder", "9");
-    }
-
     // If Velocity's natives are being extracted to a different temporary directory, make sure the
     // Netty natives are extracted there as well
     if (System.getProperty("velocity.natives-tmpdir") != null) {
