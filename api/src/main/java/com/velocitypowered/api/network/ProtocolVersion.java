5af53f2d77284557af2db7448114fb1a38286563
Five (Xer) <admin@fivepb.me>
2021-11-12 10:56:29 +0100

    Snapshot 1.18-pre1

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -56,7 +56,7 @@ public enum ProtocolVersion {
   MINECRAFT_1_16_4(754, "1.16.4", "1.16.5"),
   MINECRAFT_1_17(755, "1.17"),
   MINECRAFT_1_17_1(756, "1.17.1"),
-  MINECRAFT_1_18(-1, 48, "1.18"); // Snapshot: 21w44a, future protocol: 757
+  MINECRAFT_1_18(-1, 49, "1.18"); // Snapshot: 1.18-pre1, future protocol: 757
 
   private static final int SNAPSHOT_BIT = 30;
 
