b5cad80cfc8b743d4565ca36c1f4de94019d28a4
FivePB (Xer) <admin@fivepb.me>
2021-11-22 19:41:07 +0100

    Snapshot 1.18-pre6

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -56,7 +56,7 @@ public enum ProtocolVersion {
   MINECRAFT_1_16_4(754, "1.16.4", "1.16.5"),
   MINECRAFT_1_17(755, "1.17"),
   MINECRAFT_1_17_1(756, "1.17.1"),
-  MINECRAFT_1_18(-1, 53, "1.18"); // Snapshot: 1.18-pre5, future protocol: 757
+  MINECRAFT_1_18(-1, 54, "1.18"); // Snapshot: 1.18-pre6, future protocol: 757
 
   private static final int SNAPSHOT_BIT = 30;
 
