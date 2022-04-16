f99e4084335b9dd91f706cb9c2f4e9717cf1164d
Daniel <dnorris443@gmail.com>
2022-04-17 00:53:48 +0100

    feat(command): add forge argument properties and serializers

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/brigadier/ArgumentPropertyRegistry.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/brigadier/ArgumentPropertyRegistry.java
@@ -238,6 +238,21 @@ public class ArgumentPropertyRegistry {
     // Crossstitch support
     register(id("crossstitch:mod_argument", mapSet(MINECRAFT_1_19, -256)), ModArgumentProperty.class, MOD);
 
+    // Forge support
+    register("forge:enum", EnumArgumentProperty.class, EnumArgumentPropertySerializer.ENUM);
+    register("forge:modid", ModIdArgumentProperty.class,
+            new ArgumentPropertySerializer<>() {
+              @Override
+              public ModIdArgumentProperty deserialize(ByteBuf buf) {
+                return new ModIdArgumentProperty();
+              }
+
+              @Override
+              public void serialize(ModIdArgumentProperty object, ByteBuf buf) {
+
+              }
+            });
+
     empty(id("minecraft:nbt")); // No longer in 1.19+
   }
 }
---
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/brigadier/EnumArgumentPropertySerializer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 Velocity Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.velocitypowered.proxy.protocol.packet.brigadier;
+
+import com.velocitypowered.proxy.protocol.ProtocolUtils;
+import io.netty.buffer.ByteBuf;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+/**
+ * An argument property serializer that will serialize and deserialize nothing.
+ */
+class EnumArgumentPropertySerializer implements ArgumentPropertySerializer<EnumArgumentProperty> {
+
+  static final EnumArgumentPropertySerializer ENUM = new EnumArgumentPropertySerializer();
+
+  private EnumArgumentPropertySerializer() {
+  }
+
+  @Override
+  public @Nullable EnumArgumentProperty deserialize(ByteBuf buf) {
+    return new EnumArgumentProperty(ProtocolUtils.readString(buf));
+  }
+
+  @Override
+  public void serialize(EnumArgumentProperty object, ByteBuf buf) {
+    ProtocolUtils.writeString(buf, object.getClassName());
+  }
+}
