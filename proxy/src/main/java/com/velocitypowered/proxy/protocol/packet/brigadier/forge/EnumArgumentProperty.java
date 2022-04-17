6fdbd852247ed019095f63ee5d6559f0c8a65956
Daniel <dnorris443@gmail.com>
2022-04-17 22:51:23 +0100

    ref(proxy): update formatting for gradle style check

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/brigadier/forge/EnumArgumentProperty.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/brigadier/forge/EnumArgumentProperty.java
@@ -23,18 +23,18 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
 public class EnumArgumentProperty implements ArgumentType<String> {
 
-    private final String className;
+  private final String className;
 
-    public EnumArgumentProperty(String className) {
-        this.className = className;
-    }
+  public EnumArgumentProperty(String className) {
+    this.className = className;
+  }
 
-    public String getClassName() {
-        return this.className;
-    }
+  public String getClassName() {
+    return this.className;
+  }
 
-    @Override
-    public String parse(StringReader reader) throws CommandSyntaxException {
-        return reader.readUnquotedString();
-    }
+  @Override
+  public String parse(StringReader reader) throws CommandSyntaxException {
+    return reader.readUnquotedString();
+  }
 }
