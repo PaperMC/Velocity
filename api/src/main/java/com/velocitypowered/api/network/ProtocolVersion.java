0f203792f277d32028d70067aacb97b2b84b15b9
FivePB <admin@fivepb.me>
2022-05-04 20:48:40 +0200

    Snapshot 22w16b

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -58,7 +58,7 @@ public enum ProtocolVersion {
   MINECRAFT_1_17_1(756, "1.17.1"),
   MINECRAFT_1_18(757, "1.18", "1.18.1"),
   MINECRAFT_1_18_2(758, "1.18.2"),
-  MINECRAFT_1_19(-1, 80, "1.19"); // Snapshot 22w16a
+  MINECRAFT_1_19(-1, 81, "1.19"); // Snapshot 22w16b
 
   private static final int SNAPSHOT_BIT = 30;
 
