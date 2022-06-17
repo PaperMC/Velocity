c51789611d896a109cb1c890bf97a7ed21204320
STG-Allen <ajacob715@gmail.com>
2022-06-17 11:53:37 -0400

    Improve plugin id and dependency id handling

--- api/src/ap/java/com/velocitypowered/api/plugin/ap/SerializedPluginDescription.java
+++ api/src/ap/java/com/velocitypowered/api/plugin/ap/SerializedPluginDescription.java
@@ -13,6 +13,7 @@ import com.google.common.collect.ImmutableList;
 import com.velocitypowered.api.plugin.Plugin;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
@@ -21,7 +22,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 public final class SerializedPluginDescription {
 
-  public static final Pattern ID_PATTERN = Pattern.compile("[a-z][a-z0-9-_]{0,63}");
+  public static final Pattern ID_PATTERN = Pattern.compile("[a-z-A-Z][a-z-A-Z-0-9-_]{0,63}");
 
   // @Nullable is used here to make GSON skip these in the serialized file
   private final String id;
@@ -38,7 +39,7 @@ public final class SerializedPluginDescription {
       List<String> authors, List<Dependency> dependencies, String main) {
     Preconditions.checkNotNull(id, "id");
     Preconditions.checkArgument(ID_PATTERN.matcher(id).matches(), "id is not valid");
-    this.id = id;
+    this.id = id.toLowerCase(Locale.ROOT);
     this.name = Strings.emptyToNull(name);
     this.version = Strings.emptyToNull(version);
     this.description = Strings.emptyToNull(description);
@@ -52,7 +53,7 @@ public final class SerializedPluginDescription {
   static SerializedPluginDescription from(Plugin plugin, String qualifiedName) {
     List<Dependency> dependencies = new ArrayList<>();
     for (com.velocitypowered.api.plugin.Dependency dependency : plugin.dependencies()) {
-      dependencies.add(new Dependency(dependency.id(), dependency.optional()));
+      dependencies.add(new Dependency(dependency.id().toLowerCase(Locale.ROOT), dependency.optional()));
     }
     return new SerializedPluginDescription(plugin.id(), plugin.name(), plugin.version(),
         plugin.description(), plugin.url(),
