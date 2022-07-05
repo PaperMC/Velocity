1bf8dba2ebb73b6ab219bcfab89440840fffa363
Loremente <Loremente@outlook.it>
2022-07-05 19:04:19 +0200

    Update command server and remove aliases for avoid conflicts
    Update command server with a command that no proxy has so far, and to avoid conflicts with spigot / paper remove the shutdown aliases.

--- proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
@@ -209,9 +209,8 @@ public class VelocityServer implements ProxyServer, ForwardingAudience {
 
     // Initialize commands first
     commandManager.register("velocity", new VelocityCommand(this));
-    commandManager.register("server", new ServerCommand(this));
-    commandManager.register("shutdown", ShutdownCommand.command(this),
-        "end", "stop");
+    commandManager.register("goto", new ServerCommand(this));
+    commandManager.register("shutdown", ShutdownCommand.command(this));
     new GlistCommand(this).register();
 
     this.doStartupConfigLoad();
