cf99dcb09d91ce23efe3ab214e1c89f97869039f
Hugo Manrique <contact@hugmanrique.me>
2022-07-19 15:41:29 +0200

    Simplify tests

--- proxy/src/test/java/com/velocitypowered/proxy/config/VelocityConfigTests.java
+++ proxy/src/test/java/com/velocitypowered/proxy/config/VelocityConfigTests.java
@@ -1,25 +1,30 @@
 package com.velocitypowered.proxy.config;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VelocityConfigTests {
 
-  // Test file creation
   private static final Path CONFIG_PATH = Path.of("velocity.toml");
   private static final Path SECRET_PATH = Path.of("forwarding.secret");
 
   @AfterEach
   void tearDown() throws IOException {
-    Files.delete(CONFIG_PATH);
-    Files.delete(SECRET_PATH);
+    Files.deleteIfExists(CONFIG_PATH);
+    Files.deleteIfExists(SECRET_PATH);
   }
+
+  // Test file creation on first startup
 
   @Test
   void createsFilesOnFirstStartup() throws IOException {
@@ -27,30 +32,46 @@ public class VelocityConfigTests {
 
     assertTrue(Files.exists(CONFIG_PATH));
     assertTrue(Files.exists(SECRET_PATH));
+    assertTrue(config.getForwardingSecret().length > 0);
     assertArrayEquals(Files.readAllBytes(SECRET_PATH), config.getForwardingSecret());
   }
+
+  @Test
+  void respectsSecretFileOnFirstStartup() throws IOException {
+    Files.writeString(SECRET_PATH, "foo");
+    final var config = VelocityConfiguration.read(CONFIG_PATH);
+
+    assertArrayEquals("foo".getBytes(StandardCharsets.UTF_8), config.getForwardingSecret());
+  }
+
+  @Test
+  @SetEnvironmentVariable(key = "VELOCITY_FORWARDING_SECRET", value = "baz")
+  void doesNotCreateSecretFileWhenEnvVarIsSet() throws IOException {
+    final var config = VelocityConfiguration.read(CONFIG_PATH);
+
+    assertFalse(Files.exists(SECRET_PATH));
+    assertArrayEquals("baz".getBytes(StandardCharsets.UTF_8), config.getForwardingSecret());
+  }
+
+  // Test file creation on subsequent startups
 
   @Test
   void createsSecretFileWhenMissing() throws IOException {
     VelocityConfiguration.read(CONFIG_PATH);
     Files.delete(SECRET_PATH);
-
     final var config = VelocityConfiguration.read(CONFIG_PATH);
-    assertTrue(Files.exists(SECRET_PATH));
-    assertArrayEquals(Files.readAllBytes(SECRET_PATH), config.getForwardingSecret());
-  }
 
-  @Test
-  void doesNotCreateSecretFileWhenEnvVarIsSet() {
-    // todo: include junit-pioneer or system-stubs
+    assertTrue(Files.exists(SECRET_PATH));
+    assertTrue(config.getForwardingSecret().length > 0);
+    assertArrayEquals(Files.readAllBytes(SECRET_PATH), config.getForwardingSecret());
   }
 
   @Test
   void recreatesSecretFileWhenEmpty() throws IOException {
     VelocityConfiguration.read(CONFIG_PATH);
-    Files.writeString(SECRET_PATH, "");
-
+    Files.writeString(SECRET_PATH, ""); // truncates existing
     final var config = VelocityConfiguration.read(CONFIG_PATH);
+
     assertTrue(config.getForwardingSecret().length > 0);
     assertArrayEquals(Files.readAllBytes(SECRET_PATH), config.getForwardingSecret());
   }
