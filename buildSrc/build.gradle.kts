03b5160066f40fe5c67f2f285ebebd3a6f370f44
Andrew Steinborn <git@steinborn.me>
2023-01-01 17:27:08 -0500

    Remove references to Gradle plugins I did not copy over

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,10 +9,6 @@ repositories {
     maven("https://plugins.gradle.org/m2")
 }
 
-//dependencies {
-//    implementation("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:2.0.1")
-//}
-
 gradlePlugin {
     plugins {
         register("set-manifest-impl-version") {
@@ -23,14 +19,6 @@ gradlePlugin {
             id = "velocity-checkstyle"
             implementationClass = "com.velocitypowered.script.VelocityCheckstylePlugin"
         }
-//        register("velocity-errorprone") {
-//            id = "velocity-errorprone"
-//            implementationClass = "com.velocitypowered.script.VelocityErrorPronePlugin"
-//        }
-//        register("velocity-javadoc") {
-//            id = "velocity-javadoc"
-//            implementationClass = "com.velocitypowered.script.VelocityErrorPronePlugin"
-//        }
         register("velocity-publish") {
             id = "velocity-publish"
             implementationClass = "com.velocitypowered.script.VelocityPublishPlugin"
