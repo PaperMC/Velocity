06e19ec1853b559b637bb6020851c64454b210cc
Gero <gecam59@gmail.com>
2023-03-20 11:20:01 +0100

    Remove registry decoding

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/ClientPlaySessionHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/ClientPlaySessionHandler.java
@@ -473,7 +473,7 @@ public class ClientPlaySessionHandler implements MinecraftSessionHandler {
       }
     }
 
-    destination.setActiveDimensionRegistry(joinGame.getDimensionRegistry()); // 1.16
+    destination.setActiveDimensionRegistry(joinGame.getRegistry()); // 1.16
 
     // Remove previous boss bars. These don't get cleared when sending JoinGame, thus the need to
     // track them.
