184202173221734f30f45fdba000f5e4a20b9456
powercas_gamer <cas@deltapvp.net>
2023-06-25 13:29:28 +0200

    clarify what file has to be created

--- proxy/src/main/java/com/velocitypowered/proxy/config/VelocityConfiguration.java
+++ proxy/src/main/java/com/velocitypowered/proxy/config/VelocityConfiguration.java
@@ -499,7 +499,7 @@ public class VelocityConfiguration implements ProxyConfig {
                 "The file " + forwardSecretFile + " is not a valid file or it is a directory.");
           }
         } else {
-          throw new RuntimeException("The forwarding-secret-file does not exist.");
+          throw new RuntimeException("The file '" + secretPath + "' does not exist.");
         }
       }
     }
