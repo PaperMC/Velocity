f62d75989698b9efee51cdd8e59be0a4b587f3ed
Andrew Steinborn <git@steinborn.me>
2023-08-17 23:51:34 -0400

    Do not track plugin channels registered per-player on the proxy (#591)
    We don't need to track this information since Velocity uses the JoinGame packet, which is about as good of a server rejoin mechanism we're likely to get in vanilla Minecraft.

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/ConnectedPlayer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/ConnectedPlayer.java
@@ -77,12 +77,10 @@ import com.velocitypowered.proxy.tablist.VelocityTabList;
 import com.velocitypowered.proxy.tablist.VelocityTabListLegacy;
 import com.velocitypowered.proxy.util.ClosestLocaleMatcher;
 import com.velocitypowered.proxy.util.DurationUtils;
-import com.velocitypowered.proxy.util.collect.CappedSet;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import java.net.InetSocketAddress;
 import java.util.ArrayDeque;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -155,7 +153,6 @@ public class ConnectedPlayer implements MinecraftConnectionAssociation, Player, 
   private final InternalTabList tabList;
   private final VelocityServer server;
   private ClientConnectionPhase connectionPhase;
-  private final Collection<String> knownChannels;
   private final CompletableFuture<Void> teardownFuture = new CompletableFuture<>();
   private @MonotonicNonNull List<String> serversToTry = null;
   private @MonotonicNonNull Boolean previousResourceResponse;
@@ -185,7 +182,6 @@ public class ConnectedPlayer implements MinecraftConnectionAssociation, Player, 
     this.virtualHost = virtualHost;
     this.permissionFunction = PermissionFunction.ALWAYS_UNDEFINED;
     this.connectionPhase = connection.getType().getInitialClientPhase();
-    this.knownChannels = CappedSet.create(MAX_PLUGIN_CHANNELS);
     this.onlineMode = onlineMode;
 
     if (connection.getProtocolVersion().compareTo(ProtocolVersion.MINECRAFT_1_19_3) >= 0) {
@@ -1101,15 +1097,6 @@ public class ConnectedPlayer implements MinecraftConnectionAssociation, Player, 
    */
   public void setPhase(ClientConnectionPhase connectionPhase) {
     this.connectionPhase = connectionPhase;
-  }
-
-  /**
-   * Return all the plugin message channels "known" to the client.
-   *
-   * @return the channels
-   */
-  public Collection<String> getKnownChannels() {
-    return knownChannels;
   }
 
   @Override
