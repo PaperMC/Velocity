7f8edf8c77f9622a71b9904199aea0bf87f82ebe
Andrew Steinborn <git@steinborn.me>
2023-10-28 19:53:49 -0400

    Fix typo in `VelocityRegisteredServer#ping()`

--- proxy/src/main/java/com/velocitypowered/proxy/server/VelocityRegisteredServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/server/VelocityRegisteredServer.java
@@ -127,7 +127,7 @@ public class VelocityRegisteredServer implements RegisteredServer, ForwardingAud
         PingSessionHandler handler = new PingSessionHandler(pingFuture,
             VelocityRegisteredServer.this, conn, pingOptions.getProtocolVersion());
         conn.setActiveSessionHandler(StateRegistry.HANDSHAKE, handler);
-        conn.addSessionHandler(StateRegistry.LOGIN, handler);
+        conn.addSessionHandler(StateRegistry.STATUS, handler);
       } else {
         pingFuture.completeExceptionally(future.cause());
       }
