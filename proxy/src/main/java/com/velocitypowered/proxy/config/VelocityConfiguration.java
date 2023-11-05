1d39389fd45beb514e9202b63fe7b427f8125205
冰砚炽 <Bing_Yanchi@outlook.com>
2023-11-05 12:13:41 +0800

    Add -haproxy-protocol startup parameter

--- proxy/src/main/java/com/velocitypowered/proxy/config/VelocityConfiguration.java
+++ proxy/src/main/java/com/velocitypowered/proxy/config/VelocityConfiguration.java
@@ -352,6 +352,10 @@ public class VelocityConfiguration implements ProxyConfig {
     return advanced.isProxyProtocol();
   }
 
+  public void setProxyProtocol(boolean proxyProtocol) {
+    advanced.setProxyProtocol(proxyProtocol);
+  }
+
   public boolean useTcpFastOpen() {
     return advanced.tcpFastOpen;
   }
@@ -807,6 +811,10 @@ public class VelocityConfiguration implements ProxyConfig {
 
     public boolean isProxyProtocol() {
       return proxyProtocol;
+    }
+
+    public void setProxyProtocol(boolean proxyProtocol) {
+      this.proxyProtocol = proxyProtocol;
     }
 
     public boolean isTcpFastOpen() {
