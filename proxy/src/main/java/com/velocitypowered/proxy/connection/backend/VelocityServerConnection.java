4080ee2eaa8479c241902860d53d6575f249623d
Riley Park <rileysebastianpark@gmail.com>
2024-01-28 19:05:21 -0800

    Extract player data forwarding to separate class (#1210)
    * Extract player data forwarding to separate class
    
    * Use modern switch statement

--- proxy/src/main/java/com/velocitypowered/proxy/connection/backend/VelocityServerConnection.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/backend/VelocityServerConnection.java
@@ -17,24 +17,22 @@
 
 package com.velocitypowered.proxy.connection.backend;
 
-import static com.velocitypowered.proxy.VelocityServer.GENERAL_GSON;
 import static com.velocitypowered.proxy.connection.forge.legacy.LegacyForgeConstants.HANDSHAKE_HOSTNAME_TOKEN;
 import static com.velocitypowered.proxy.network.Connections.HANDLER;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
 import com.velocitypowered.api.network.ProtocolVersion;
 import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.messages.ChannelIdentifier;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import com.velocitypowered.api.proxy.server.ServerInfo;
-import com.velocitypowered.api.util.GameProfile.Property;
 import com.velocitypowered.proxy.VelocityServer;
 import com.velocitypowered.proxy.config.PlayerInfoForwarding;
 import com.velocitypowered.proxy.connection.ConnectionTypes;
 import com.velocitypowered.proxy.connection.MinecraftConnection;
 import com.velocitypowered.proxy.connection.MinecraftConnectionAssociation;
 import com.velocitypowered.proxy.connection.MinecraftSessionHandler;
+import com.velocitypowered.proxy.connection.PlayerDataForwarding;
 import com.velocitypowered.proxy.connection.client.ConnectedPlayer;
 import com.velocitypowered.proxy.connection.forge.modern.ModernForgeConnectionType;
 import com.velocitypowered.proxy.connection.util.ConnectionRequestResults.Impl;
@@ -47,13 +45,10 @@ import com.velocitypowered.proxy.server.VelocityRegisteredServer;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.UnaryOperator;
 import net.kyori.adventure.nbt.CompoundBinaryTag;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -142,32 +137,23 @@ public class VelocityServerConnection implements MinecraftConnectionAssociation,
     }
   }
 
-  private String createLegacyForwardingAddress(UnaryOperator<List<Property>> propertiesTransform) {
-    // BungeeCord IP forwarding is simply a special injection after the "address" in the handshake,
-    // separated by \0 (the null byte). In order, you send the original host, the player's IP, their
-    // UUID (undashed), and if you are in online-mode, their login properties (from Mojang).
-    StringBuilder data = new StringBuilder().append(proxyPlayer.getVirtualHost().orElseGet(() ->
-                    registeredServer.getServerInfo().getAddress()).getHostString())
-        .append('\0')
-        .append(getPlayerRemoteAddressAsString())
-        .append('\0')
-        .append(proxyPlayer.getGameProfile().getUndashedId())
-        .append('\0');
-    GENERAL_GSON
-        .toJson(propertiesTransform.apply(proxyPlayer.getGameProfile().getProperties()), data);
-    return data.toString();
-  }
-
   private String createLegacyForwardingAddress() {
-    return createLegacyForwardingAddress(UnaryOperator.identity());
+    return PlayerDataForwarding.createLegacyForwardingAddress(
+      proxyPlayer.getVirtualHost().orElseGet(() ->
+        registeredServer.getServerInfo().getAddress()).getHostString(),
+      getPlayerRemoteAddressAsString(),
+      proxyPlayer.getGameProfile()
+    );
   }
 
   private String createBungeeGuardForwardingAddress(byte[] forwardingSecret) {
-    // Append forwarding secret as a BungeeGuard token.
-    Property property =
-        new Property("bungeeguard-token", new String(forwardingSecret, StandardCharsets.UTF_8), "");
-    return createLegacyForwardingAddress(
-        properties -> ImmutableList.<Property>builder().addAll(properties).add(property).build());
+    return PlayerDataForwarding.createBungeeGuardForwardingAddress(
+      proxyPlayer.getVirtualHost().orElseGet(() ->
+        registeredServer.getServerInfo().getAddress()).getHostString(),
+      getPlayerRemoteAddressAsString(),
+      proxyPlayer.getGameProfile(),
+      forwardingSecret
+    );
   }
 
   private void startHandshake() {
