b2a7ebcd49f1bd631600641cba6565438cf2d949
Maxim Breitman <89523915+maximbreitman@users.noreply.github.com>
2024-02-29 22:12:21 +0300

    Fixed disconnection reason display due to outdated client version

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/HandshakeSessionHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/HandshakeSessionHandler.java
@@ -118,6 +118,8 @@ public class HandshakeSessionHandler implements MinecraftSessionHandler {
 
   private void handleLogin(HandshakePacket handshake, InitialInboundConnection ic) {
     if (!ProtocolVersion.isSupported(handshake.getProtocolVersion())) {
+      // Bump connection into correct protocol state so that we can send the disconnect packet.
+      connection.setState(StateRegistry.LOGIN);
       ic.disconnectQuietly(Component.translatable()
               .key("multiplayer.disconnect.outdated_client")
               .arguments(Component.text(ProtocolVersion.SUPPORTED_VERSION_STRING))
