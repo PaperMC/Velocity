a3cce276d4772594076c7d4170a13775096ad6d5
Timongcraft <timong.seidel@gmail.com>
2024-10-23 15:38:13 +0200

    Update 1.21.2 protocol to include 1.21.3 release

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -88,7 +88,7 @@ public enum ProtocolVersion implements Ordered<ProtocolVersion> {
   MINECRAFT_1_20_3(765, "1.20.3", "1.20.4"),
   MINECRAFT_1_20_5(766, "1.20.5", "1.20.6"),
   MINECRAFT_1_21(767, "1.21", "1.21.1"),
-  MINECRAFT_1_21_2(768, "1.21.2");
+  MINECRAFT_1_21_2(768, "1.21.2", "1.21.3");
 
   private static final int SNAPSHOT_BIT = 30;
 
