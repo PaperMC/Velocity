088b0f885042fe9ce292ac7e482cd0311550e308
riku6460 <17585784+riku6460@users.noreply.github.com>
2024-12-01 07:24:41 +0900

    Fix callback command not working
    Broken in https://github.com/PaperMC/Velocity/commit/2016d1482f185daf4b92b6775fb4a8d2ef1e0da3#diff-1217e64751ab6522195a858620cceeb4f2d2b6506c36dd4e396726ad3e7ef0cb

--- proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
@@ -253,7 +253,7 @@ public class VelocityServer implements ProxyServer, ForwardingAudience {
         commandManager.metaBuilder(callbackCommand)
             .plugin(VelocityVirtualPlugin.INSTANCE)
             .build(),
-        velocityParentCommand
+        callbackCommand
     );
     final BrigadierCommand serverCommand = ServerCommand.create(this);
     commandManager.register(
