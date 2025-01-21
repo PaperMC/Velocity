34a8d42098af40bacefae85d03c8403141dc0f84
Vankka <vankka.main@gmail.com>
2025-01-21 17:51:26 +0200

    Fix checkstyle error

--- api/src/main/java/com/velocitypowered/api/event/PostOrder.java
+++ api/src/main/java/com/velocitypowered/api/event/PostOrder.java
@@ -15,6 +15,8 @@ public enum PostOrder {
   FIRST, EARLY, NORMAL, LATE, LAST,
 
   /**
+   * Previously used to specify that {@link Subscribe#priority()} should be used.
+   *
    * @deprecated No longer required, you only need to specify {@link Subscribe#priority()}.
    */
   @Deprecated
