5d900177dcfac8df64b7fab8b13e65d71ef0af97
Timon Seidel <timong.seidel@gmail.com>
2025-04-02 19:52:50 +0200

    chore: refine javadoc

--- api/src/main/java/com/velocitypowered/api/event/player/PlayerClientLoadedWorldEvent.java
+++ api/src/main/java/com/velocitypowered/api/event/player/PlayerClientLoadedWorldEvent.java
@@ -13,11 +13,13 @@ import com.velocitypowered.api.proxy.Player;
 import java.time.Duration;
 
 /**
- * Called when a player is marked as loaded.
+ * Called when a player is marked as loaded by the client.
  *
  * <p>Note: This event may be fired in the following scenarios:
  * <br>- If the player notifies the server after loading the world (closing the downloading terrain screen)
- * <br>- If the player has <u>not</u> notified the server within 60 ticks after joining the server - ({@link #timeout} = true)
+ * <br>- If the player has <u>not</u> notified the server within 1500ms after joining the server - ({@link #timeout} = true)
+ *
+ * <p>Note: Velocity does not ensure the timing of this packet. Consequently, this event might fire before {@link Player#getCurrentServer()} is set.
  */
 @Beta
 public final class PlayerClientLoadedWorldEvent {
@@ -38,7 +40,7 @@ public final class PlayerClientLoadedWorldEvent {
 
   /**
    * True if the event was triggered because the server has not been notified by the player
-   * for 60 ticks after the player joined the server.
+   * for 1500ms after the player joined the server.
    *
    * @return true if the event was triggered because of a timeout
    */
