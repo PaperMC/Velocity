38b01eb6718f63ae3d51e429f9cb4ea95d7317bd
Gero <gecam59@gmail.com>
2025-06-02 15:10:53 +0200

    forward restricted flag

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/AvailableCommandsPacket.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/AvailableCommandsPacket.java
@@ -50,12 +50,14 @@ import java.util.Deque;
 import java.util.Iterator;
 import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class AvailableCommandsPacket implements MinecraftPacket {
 
   private static final Command<CommandSource> PLACEHOLDER_COMMAND = source -> 0;
+  private static final Predicate<CommandSource> PLACEHOLDER_REQUIREMENT = source -> true;
 
   private static final byte NODE_TYPE_ROOT = 0x00;
   private static final byte NODE_TYPE_LITERAL = 0x01;
@@ -146,6 +148,9 @@ public class AvailableCommandsPacket implements MinecraftPacket {
     }
     if (node.getCommand() != null) {
       flags |= FLAG_EXECUTABLE;
+    }
+    if (node.getRequirement() == PLACEHOLDER_REQUIREMENT) {
+      flags |= FLAG_IS_RESTRICTED;
     }
 
     if (node instanceof LiteralCommandNode<?>) {
@@ -288,6 +293,11 @@ public class AvailableCommandsPacket implements MinecraftPacket {
           // If executable, add an empty command
           if ((flags & FLAG_EXECUTABLE) != 0) {
             args.executes(PLACEHOLDER_COMMAND);
+          }
+
+          // If restricted, add empty requirement
+          if ((flags & FLAG_IS_RESTRICTED) != 0) {
+            args.requires(PLACEHOLDER_REQUIREMENT);
           }
 
           this.built = args.build();
