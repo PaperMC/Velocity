10b7e1b9567d69accfee7f86098a83489e829f1d
Gero <gecam59@gmail.com>
2025-06-10 11:23:27 +0200

    1.21.6

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -91,7 +91,7 @@ public enum ProtocolVersion implements Ordered<ProtocolVersion> {
   MINECRAFT_1_21_2(768, "1.21.2", "1.21.3"),
   MINECRAFT_1_21_4(769, "1.21.4"),
   MINECRAFT_1_21_5(770, "1.21.5"),
-  MINECRAFT_1_21_6(771, 252, "1.21.6");
+  MINECRAFT_1_21_6(771, "1.21.6");
 
   private static final int SNAPSHOT_BIT = 30;
 
