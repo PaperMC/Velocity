79a7957231f620a01df8b08807067ad09283a036
DartCZ <DartCZ@users.noreply.github.com>
2025-08-13 19:56:29 +0200

    Update proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
    Co-authored-by: Timon Seidel <timong.seidel@gmail.com>

--- proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
@@ -586,7 +586,7 @@ public class VelocityServer implements ProxyServer, ForwardingAudience {
                 .toCompletableFuture()
                 .get(shutdownTimeout, TimeUnit.SECONDS);
       } catch (TimeoutException ignored) {
-        logger.warn("Your plugins took over {} seconds to shut down while disconnecting players.",
+        logger.warn("Your plugins took over {} seconds during pre shut down.",
                 shutdownTimeout);
       } catch (ExecutionException ee) {
         logger.error("Exception in ProxyPreShutdownEvent handler; continuing shutdown.", ee);
