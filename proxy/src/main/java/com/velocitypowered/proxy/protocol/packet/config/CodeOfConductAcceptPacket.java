b5a1a3cf0f7e73bb13ba72ad7f33a184721a82f2
benniofficialdev <coden.mit.benni@gmail.com>
2026-01-05 22:32:06 +0100

    Update CodeOfConductAcceptPacket.java

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/config/CodeOfConductAcceptPacket.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/config/CodeOfConductAcceptPacket.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2025 Velocity Contributors
+ * Copyright (C) 2018-2026 Velocity Contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
