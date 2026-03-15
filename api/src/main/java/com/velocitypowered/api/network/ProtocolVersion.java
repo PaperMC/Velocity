e5222ad74f4f89dd5444b1df1db793e07ee9fafa
Aaron <bauhd@gmx.net>
2026-03-15 22:56:26 +0100

    26.1-pre-1

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -95,7 +95,7 @@ public enum ProtocolVersion implements Ordered<ProtocolVersion> {
   MINECRAFT_1_21_7(772, "1.21.7", "1.21.8"),
   MINECRAFT_1_21_9(773, "1.21.9", "1.21.10"),
   MINECRAFT_1_21_11(774, "1.21.11"),
-  MINECRAFT_26_1(-1, 297, "26.1-snapshot-11");
+  MINECRAFT_26_1(-1, 298, "26.1-pre-1");
 
   private static final int SNAPSHOT_BIT = 30;
 
