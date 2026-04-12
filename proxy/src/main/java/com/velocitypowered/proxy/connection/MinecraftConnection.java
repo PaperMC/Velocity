d734105af24b1e071d429a86b8a390ed8b351483
booky <booky@booky.dev>
2026-04-12 15:24:10 +0200

    Use EnumMap implementation for session handler map

--- proxy/src/main/java/com/velocitypowered/proxy/connection/MinecraftConnection.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/MinecraftConnection.java
@@ -67,7 +67,7 @@ import io.netty.util.ReferenceCountUtil;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.security.GeneralSecurityException;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -109,7 +109,7 @@ public class MinecraftConnection extends ChannelInboundHandlerAdapter {
     this.server = server;
     this.state = StateRegistry.HANDSHAKE;
 
-    this.sessionHandlers = new HashMap<>();
+    this.sessionHandlers = new EnumMap<>(StateRegistry.class);
   }
 
   @Override
