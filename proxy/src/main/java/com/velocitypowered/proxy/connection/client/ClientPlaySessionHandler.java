b7cb35e9abfb8ab21f7425675051078b000f6af8
Wouter Gritter <wouter@gritter.nl>
2026-05-24 14:55:47 +0200

    Clear counters once as the entire queue will have been processed

--- proxy/src/main/java/com/velocitypowered/proxy/connection/client/ClientPlaySessionHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/client/ClientPlaySessionHandler.java
@@ -677,10 +677,10 @@ public class ClientPlaySessionHandler implements MinecraftSessionHandler {
     // If we had plugin messages queued during login/FML handshake, send them now.
     PluginMessagePacket pm;
     while ((pm = loginPluginMessages.poll()) != null) {
-      loginPluginMessagesBytes.addAndGet(-pm.content().readableBytes());
-      loginPluginMessagesCount.decrementAndGet();
       serverMc.delayedWrite(pm);
     }
+    loginPluginMessagesBytes.set(0);
+    loginPluginMessagesCount.set(0);
 
     // Clear any title from the previous server.
     if (player.getProtocolVersion().noLessThan(ProtocolVersion.MINECRAFT_1_8)) {
@@ -911,10 +911,10 @@ public class ClientPlaySessionHandler implements MinecraftSessionHandler {
       if (connection != null) {
         PluginMessagePacket pm;
         while ((pm = loginPluginMessages.poll()) != null) {
-          loginPluginMessagesBytes.addAndGet(-pm.content().readableBytes());
-          loginPluginMessagesCount.decrementAndGet();
           connection.write(pm);
         }
+        loginPluginMessagesBytes.set(0);
+        loginPluginMessagesCount.set(0);
       }
     }
   }
