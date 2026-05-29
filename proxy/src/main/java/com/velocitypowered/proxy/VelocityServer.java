a8c05a69fbc181d2c08f22b6e9e4d1104afe12b2
Park <park@junhyung.kr>
2026-05-30 03:51:03 +0900

    Add CLI options to load plugins from extra jars and directories

--- proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
@@ -426,15 +426,14 @@ public class VelocityServer implements ProxyServer, ForwardingAudience {
 
       if (!pluginPath.toFile().exists()) {
         Files.createDirectory(pluginPath);
-      } else {
-        if (!pluginPath.toFile().isDirectory()) {
-          logger.warn("Plugin location {} is not a directory, continuing without loading plugins",
-              pluginPath);
-          return;
-        }
-
-        pluginManager.loadPlugins(pluginPath);
+      } else if (!pluginPath.toFile().isDirectory()) {
+        logger.warn("Plugin location {} is not a directory, continuing without loading plugins",
+            pluginPath);
+        return;
       }
+
+      pluginManager.loadPlugins(pluginPath, options.getExtraPluginDirectories(),
+          options.getExtraPluginJars());
     } catch (Exception e) {
       logger.error("Couldn't load plugins", e);
     }
