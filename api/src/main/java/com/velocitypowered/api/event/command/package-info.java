87d993b205b9a7373c17bdd2c06d33f7f4114c13
Wouter Gritter <wouter@gritter.nl>
2026-06-08 20:40:45 +0200

    Add missing `package-info.java`s for event packages

---
+++ api/src/main/java/com/velocitypowered/api/event/command/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018 Velocity Contributors
+ *
+ * The Velocity API is licensed under the terms of the MIT License. For more details,
+ * reference the LICENSE file in the api top-level directory.
+ */
+
+/**
+ * Provides events for handling command execution.
+ */
+package com.velocitypowered.api.event.command;
---
+++ api/src/main/java/com/velocitypowered/api/event/player/configuration/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018 Velocity Contributors
+ *
+ * The Velocity API is licensed under the terms of the MIT License. For more details,
+ * reference the LICENSE file in the api top-level directory.
+ */
+
+/**
+ * Provides events for handling the player configuration phase.
+ */
+package com.velocitypowered.api.event.player.configuration;
