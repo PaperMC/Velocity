65207e2583b76102df4db649d27a4490916153b7
Aaron <bauhd@gmx.net>
2026-06-12 15:21:55 +0200

    26.2-rc-2

--- api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
+++ api/src/main/java/com/velocitypowered/api/network/ProtocolVersion.java
@@ -96,7 +96,7 @@ public enum ProtocolVersion implements Ordered<ProtocolVersion> {
   MINECRAFT_1_21_9(773, "1.21.9", "1.21.10"),
   MINECRAFT_1_21_11(774, "1.21.11"),
   MINECRAFT_26_1(775, "26.1", "26.1.1", "26.1.2"),
-  MINECRAFT_26_2(-1, 321, "26.2-rc-1");
+  MINECRAFT_26_2(-1, 322, "26.2-rc-2");
 
   private static final int SNAPSHOT_BIT = 30;
 
