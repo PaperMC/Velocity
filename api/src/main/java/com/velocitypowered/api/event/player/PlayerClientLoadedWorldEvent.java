f76a927a645cacd2633aa2fd23eceb22e243b922
Timon Seidel <timong.seidel@gmail.com>
2026-07-30 21:42:10 +0200

    chore(license): bump year
    Co-authored-by: Wouter Gritter <wouter@gritter.nl>

--- api/src/main/java/com/velocitypowered/api/event/player/PlayerClientLoadedWorldEvent.java
+++ api/src/main/java/com/velocitypowered/api/event/player/PlayerClientLoadedWorldEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2025 Velocity Contributors
+ * Copyright (C) 2018-2026 Velocity Contributors
  *
  * The Velocity API is licensed under the terms of the MIT License. For more details,
  * reference the LICENSE file in the api top-level directory.
