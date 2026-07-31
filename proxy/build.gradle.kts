f4f7f7fcc2531e7e8a2e474585b42ca969f88052
Wouter Gritter <wouter@gritter.nl>
2026-07-31 23:10:48 +0200

    Bump fastutil to 8.5.19 and remove exclusions

--- proxy/build.gradle.kts
+++ proxy/build.gradle.kts
@@ -33,65 +33,6 @@ tasks {
 
         transform(Log4j2PluginsCacheFileTransformer::class.java)
 
-        // Exclude all the collection types we don"t intend to use
-        exclude("it/unimi/dsi/fastutil/booleans/**")
-        exclude("it/unimi/dsi/fastutil/bytes/**")
-        exclude("it/unimi/dsi/fastutil/chars/**")
-        exclude("it/unimi/dsi/fastutil/doubles/**")
-        exclude("it/unimi/dsi/fastutil/floats/**")
-        exclude("it/unimi/dsi/fastutil/longs/**")
-        exclude("it/unimi/dsi/fastutil/shorts/**")
-
-        // Exclude the fastutil IO utilities - we don"t use them.
-        exclude("it/unimi/dsi/fastutil/io/**")
-
-        // Exclude most of the int types - Object2IntMap have a values() method that returns an
-        // IntCollection, and we need Int2ObjectMap
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Boolean*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Byte*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Char*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Double*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Float*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Int*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Long*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Short*")
-        exclude("it/unimi/dsi/fastutil/ints/*Int2Reference*")
-        exclude("it/unimi/dsi/fastutil/ints/IntAVL*")
-        exclude("it/unimi/dsi/fastutil/ints/IntArrayF*")
-        exclude("it/unimi/dsi/fastutil/ints/IntArrayI*")
-        exclude("it/unimi/dsi/fastutil/ints/IntArrayL*")
-        exclude("it/unimi/dsi/fastutil/ints/IntArrayP*")
-        exclude("it/unimi/dsi/fastutil/ints/IntArraySet*")
-        exclude("it/unimi/dsi/fastutil/ints/*IntBi*")
-        exclude("it/unimi/dsi/fastutil/ints/Int*Pair")
-        exclude("it/unimi/dsi/fastutil/ints/IntLinked*")
-        exclude("it/unimi/dsi/fastutil/ints/IntList*")
-        exclude("it/unimi/dsi/fastutil/ints/IntHeap*")
-        exclude("it/unimi/dsi/fastutil/ints/IntOpen*")
-        exclude("it/unimi/dsi/fastutil/ints/IntRB*")
-        exclude("it/unimi/dsi/fastutil/ints/IntSorted*")
-        exclude("it/unimi/dsi/fastutil/ints/*Priority*")
-        exclude("it/unimi/dsi/fastutil/ints/*BigList*")
-
-        // Try to exclude everything BUT Object2Int{LinkedOpen,Open,CustomOpen}HashMap
-        exclude("it/unimi/dsi/fastutil/objects/*ObjectArray*")
-        exclude("it/unimi/dsi/fastutil/objects/*ObjectAVL*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object*Big*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Boolean*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Byte*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Char*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Double*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Float*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2IntArray*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2IntAVL*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2IntRB*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Long*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Object*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Reference*")
-        exclude("it/unimi/dsi/fastutil/objects/*Object2Short*")
-        exclude("it/unimi/dsi/fastutil/objects/*ObjectRB*")
-        exclude("it/unimi/dsi/fastutil/objects/*Reference*")
-
         // Exclude Checker Framework annotations
         exclude("org/checkerframework/checker/**")
 
